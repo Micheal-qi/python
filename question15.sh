56ac6ccb5828efb1399862658684186b1ed466b3
wang qi <161259706+Micheal-qi@users.noreply.github.com>
2024-03-06 21:56:09 +1030

    Create question15.sh

---
+++ question15.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-01.git
+cd git-practice-01
+git checkout branch1
+git add dir1
+git add file1
+git commit -m "question15.sh"
+git merge branch1
