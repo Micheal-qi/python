07e70fab2f87aaea1d41eb5e9c32bc142fdfc600
wang qi <161259706+Micheal-qi@users.noreply.github.com>
2024-03-06 21:18:03 +1030

    Update question14.sh

--- question14.sh
+++ question14.sh
@@ -1,11 +1,2 @@
 git clone https://github.com/ian-knight-uofa/git-practice-04.git
-cd git-practice-04
-git fetch
-git checkout update1
-cd git-practice-04
-git fetch
-git checkout update1
-touch question14.sh
-git add question14.sh
-git commit -m "Add commands for question14.sh"
-git push your-remote main
+git checkout --track origin/update1
