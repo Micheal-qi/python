74c38bd3243ab38b733726d6c84cfc261c200536
wang qi <161259706+Micheal-qi@users.noreply.github.com>
2024-03-06 20:21:16 +1030

    Create question14.sh

---
+++ question14.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/ian-knight-uofa/git-practice-04.git
+cd git-practice-04
+git fetch
+git checkout update1
+cd git-practice-04
+git fetch
+git checkout update1
+touch question14.sh
+git add question14.sh
+git commit -m "Add commands for question14.sh"
+git push your-remote main
