52ddd44566805a63ab4570da5821157c5f44de90
wang qi <161259706+Micheal-qi@users.noreply.github.com>
2024-03-06 20:15:42 +1030

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,8 +1,7 @@
 git checkout -b branch2
-
-echo "Content of file4" > file4
+touch file4
 git add file4
-git commit -m "Add file4 to branch2"
-
-echo "Modified content of file4" > file4
+git commit -m "Create file4"
+echo "Some content" >> file4
+git stash
 git checkout main
