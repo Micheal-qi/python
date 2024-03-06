d1da13311cbb39882c5e0774661941da27b10940
wang qi <161259706+Micheal-qi@users.noreply.github.com>
2024-03-06 20:23:19 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,2 +1,11 @@
+# Create a directory named dir2
 mkdir dir2
+
+# Move all .txt files from the current directory to dir2
 mv *.txt dir2/
+
+# Stage the changes
+git add .
+
+# Commit the changes
+git commit -m "Move text files to dir2"
